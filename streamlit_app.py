8de6608e84662df367cac0c9b6ab3bed0a59d355
venkateshanbalagan <venkatesh.anbalagan@gmail.com>
2022-10-17 11:30:10 -0400

    Update streamlit_app.py
    Added few more lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Heathier Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Heard-Boiled Free-Range Egg')
