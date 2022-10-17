79803dbd1335d62dac66a6a7c1b799efab119988
venkateshanbalagan <venkatesh.anbalagan@gmail.com>
2022-10-17 11:35:09 -0400

    Update streamlit_app.py
    Added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Heard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🍌🥭')
