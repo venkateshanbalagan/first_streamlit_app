9bd1adde8ea92437295504da3b20e4923979d3d1
venkateshanbalagan <venkatesh.anbalagan@gmail.com>
2022-10-17 11:40:33 -0400

    addded fruit macros

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My Parents New Heathier Dinner')
 
 streamlit.header('Breakfast Menu')
@@ -8,3 +9,7 @@ streamlit.text('🐔 Heard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🍌🥭')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
