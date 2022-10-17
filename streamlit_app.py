78e3a7888443485395a794059de248a2b07c0786
venkateshanbalagan <venkatesh.anbalagan@gmail.com>
2022-10-17 11:33:57 -0400

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 streamlit.title('My Parents New Heathier Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Heard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Heard-Boiled Free-Range Egg')
+streamlit.text('🥑 Avocado Toast')
